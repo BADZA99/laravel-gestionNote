d4c7c182c85f1a8d76022c76539d5164a0f28b63
papabn <pndiaye999@gmail.com>
2023-12-13 02:40:03 +0000

    Add validation messages display it in french and @csrf to add aprenant form

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => 'Le champ :attribute est obligatoire.',
+    // 'min' => [
+    //     'string'  => ':attribute doit contenir au moins :min caractères.',
+    // ],
+    // 'max' => [
+    //     'string'  => ':attribute ne peut pas contenir plus de :max caractères.',
+    // ],
+];
--- resources/views/Apprenants/create.blade.php
+++ resources/views/Apprenants/create.blade.php
@@ -11,19 +11,27 @@
     <h1 style="text-align: center; color: #4CAF50;">Formulaire ajout apprenant</h1>
 
     <form action="{{ route('store-apprenant') }}" method="POST" style="max-width: 500px; margin: auto;">
-    
-
+        @csrf
         <div style="margin-bottom: 15px;">
             <label for="nom" style="display: block; margin-bottom: 5px;">Nom</label>
             <input type="text" id="nom" name="nom" style="width: 100%; padding: 10px; border: 1px solid #ccc; border-radius: 4px;">
+            @error('nom')
+            <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
         <div style="margin-bottom: 15px;">
             <label for="prenom" style="display: block; margin-bottom: 5px;">Prenom</label>
             <input type="text" id="prenom" name="prenom" style="width: 100%; padding: 10px; border: 1px solid #ccc; border-radius: 4px;">
+            @error('prenom')
+            <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
         <div style="margin-bottom: 15px;">
             <label for="matricule" style="display: block; margin-bottom: 5px;">Matricule</label>
             <input type="text" id="matricule" name="matricule" style="width: 100%; padding: 10px; border: 1px solid #ccc; border-radius: 4px;">
+            @error('matricule')
+            <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
         <button type="submit" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer;">Ajouter</button>
     </form>
